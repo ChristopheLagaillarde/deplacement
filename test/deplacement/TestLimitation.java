a386c3dc6c9cc431c256161bc625a1c6d3e262d4
ChristopheLagaillarde <chrislag94@gmail.com>
2022-04-24 22:18:56 +0400

    ajout test case methode limiter

--- test/deplacement/TestLimitation.java
+++ test/deplacement/TestLimitation.java
@@ -7,7 +7,35 @@ class TestLimitation {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
+		// Test pour les voitures
+		Voiture toyota = new Voiture("Voiture", 0, 0, 20);
+		int vitesseActuel = toyota.limiter(10, 150);
+		assertEquals(vitesseActuel,100);
+		
+		Voiture renault = new Voiture("Voiture", 0, 0, 20);
+		vitesseActuel = renault.limiter(10, 99);
+		assertEquals(vitesseActuel,99);
+		
+		// Test pour les ascenseurs
+		Ascenseur ascenseurRapide = new Ascenseur("Ascenseur", 0, 0, 5);
+		vitesseActuel = ascenseurRapide.limiter(5, 50);
+		assertEquals(vitesseActuel,50);
+		
+		Ascenseur ascenseurLent = new Ascenseur("Ascenseur", 0, 0, 1);
+		vitesseActuel = ascenseurLent.limiter(4, 40);
+		assertEquals(vitesseActuel,40);
+
+		// Test pour les voitures volantes
+		VoitureVolante tesla = new VoitureVolante("Voiture volante", 0, 0, 1200);
+		vitesseActuel = tesla.limiter(120, 1000);
+		assertEquals(vitesseActuel,1000);
+		
+		VoitureVolante deLorean = new VoitureVolante("Voiture volante", 0, 0, 1200);
+		vitesseActuel = deLorean.limiter(90, 900);
+		assertEquals(vitesseActuel,900);
+
+
 	}
 
 }
